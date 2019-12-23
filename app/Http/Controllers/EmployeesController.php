dd45be9b33bc206e0d7295ad25d3f786509ea88f
gachiKoKa <koka4ka428@gmail.com>
2019-12-23 14:28:56 +0200

    Unnneded files removed

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Employees extends Controller
+class EmployeesController extends Controller
 {
     /**
      * Display a listing of the resource.
