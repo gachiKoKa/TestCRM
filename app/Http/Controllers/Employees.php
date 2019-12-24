aec9cd4dad76b31bd8fdbc570a69c89ad1e50795
gachiKoKa <koka4ka428@gmail.com>
2019-12-24 13:00:58 +0200

    Revert "Unnneded files removed"
    This reverts commit dd45be9b33bc206e0d7295ad25d3f786509ea88f.

--- app/Http/Controllers/Employees.php
+++ app/Http/Controllers/Employees.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class EmployeesController extends Controller
+class Employees extends Controller
 {
     /**
      * Display a listing of the resource.
